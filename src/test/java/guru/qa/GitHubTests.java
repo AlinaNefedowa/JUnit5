0f889dc717d7b397cdd9d47eda3e5f7f29930742
alinanefedowa <nefedowa.as@gmail.com>
2022-04-19 17:16:59 +0300

    added gitHub tests

---
+++ src/test/java/guru/qa/GitHubTests.java
@@ -0,0 +1,54 @@
+package guru.qa;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
+@DisplayName("Класс с тестами на GitHub")
+public class GitHubTests {
+
+    @ValueSource(strings = {
+            "Alina",
+            "Maria",
+            "Anton"
+    })
+
+    @ParameterizedTest (name = "Тесты поиск в GitHub по слову {0}")
+    void gitHubSearchTests(String testData) {
+
+        open("https://github.com");
+
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys(testData);
+        $(".header-search-input").submit();
+        $(withText(testData)).should(Condition.exist);
+    }
+
+    @CsvSource(value = {
+            "wow, Reveal CSS animation as you scroll down a page",
+            "now, Build a Jekyll blog in minutes"
+    })
+
+    @ParameterizedTest(name = "Тесты поиск в GitHub по слову {0}, ожидаем {1}")
+    void gitHubComplexSearchTests(String testData, String expectedResult) {
+
+        open("https://github.com");
+
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys(testData);
+        $(".header-search-input").submit();
+        $$(".codesearch-results")
+                .find(Condition.text(expectedResult))
+                        .shouldBe(visible);
+    }
+
+}
