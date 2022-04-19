71afabc4e3d60770f2d933b10ab3650394e7ed3e
alinanefedowa <nefedowa.as@gmail.com>
2022-04-19 17:48:42 +0300

    updated gitHub tests

--- src/test/java/guru/qa/GitHubTests.java
+++ src/test/java/guru/qa/GitHubTests.java
@@ -5,9 +5,14 @@ import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.openqa.selenium.By;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.withText;
@@ -51,4 +56,17 @@ public class GitHubTests {
                         .shouldBe(visible);
     }
 
+    static Stream<Arguments> methodSourceExampleTest() {
+        return Stream.of(
+                Arguments.of("Alina`s birthday", List.of(13, 5)),
+                Arguments.of("Anna`s birthday", List.of(4, 8))
+        );
+    }
+
+    @MethodSource("methodSourceExampleTest")
+
+    @ParameterizedTest(name = "Тесты с MethodSourceSearchTests")
+    void MethodSourceSearchTests(String first, List<Integer> second) {
+        System.out.println(first + " is " + second);
+    }
 }
